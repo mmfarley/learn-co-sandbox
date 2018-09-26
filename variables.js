a234430e1213d2ee435df11e748dcff1edc5b79c
Megan Farley <megfarley4@gmail.com>
2018-09-26 02:30:26 +0000

    automatically backed up by learn

---
+++ variables.js
@@ -0,0 +1,16 @@
+var word
+word = 'bird'
+console.log(word)
+word = 'dog'
+console.log(word)
+
+var a = 5
+var b = 2
+var c = 3
+var d = 'hello'
+var e = 'goodbye'
+console.log(a, b)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
